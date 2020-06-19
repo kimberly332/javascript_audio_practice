2c16032f1ebcd2865cdaf50b6c6cfce277ae25ff
kimberly332 <kimberly.wang1010@gmail.com>
2020-06-19 12:02:36 -0400

    added dynamic audio

--- js/main.js
+++ js/main.js
@@ -2,8 +2,18 @@
     console.log('fried!');
 
     let audioControls = document.querySelectorAll(".controls"),
-        audio = document.querySelector('audio');
+        audio = document.querySelector("audio"),
+        audioThumbs = document.querySelectorAll(".trackRef");
 
+
+    function loadAndPlay() {
+      // the 'this' keyword refers to the image you've clicked on
+      audio.src = `audio/${this.dataset.trackref}`;
+
+      // load method loads whatever resource you indicate
+      audio.load();
+      playAudio();
+    }
 
     function playAudio() {
       // play the audio track
@@ -19,4 +29,12 @@
 
     audioControls[0].addEventListener("click", playAudio);
     audioControls[1].addEventListener("click", rewindAudio);
+
+    // process the image tags and make them load the right audio track
+    for (thumb of audioThumbs) {
+      thumb.addEventListener("click", loadAndPlay);
+    }
+
+    //listen for the end of the audio tracks
+    audio.addEventListener("ended", () => console.log("track ended"));
 })();
